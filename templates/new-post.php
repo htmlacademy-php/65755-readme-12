47dae8233d2e486e0ce8f522be354f3d1458d6e1
Aleksandr Batollo <a@batollo.ru>
2021-06-14 02:27:00 +0300

    Синхронизирует изменения

--- templates/new-post.php
+++ templates/new-post.php
@@ -1,3 +1,9 @@
+<?php
+function getPostVal($name) {
+    return $_POST[$name] ?? "";
+}
+?>
+
 <div class="page__main-section">
     <div class="container">
         <h1 class="page__title page__title--adding-post">Добавить публикацию</h1>
@@ -218,7 +224,7 @@
                                         <div class="adding-post__input-wrapper form__input-wrapper">
                                             <label class="adding-post__label form__label" for="quote-heading">Заголовок <span class="form__input-required">*</span></label>
                                             <div class="form__input-section">
-                                                <input class="adding-post__input form__input" id="quote-heading" type="text" name="quote-heading" placeholder="Введите заголовок">
+                                                <input class="adding-post__input form__input" id="quote-heading" type="text" name="quote-heading" placeholder="Введите заголовок" value="<?= getPostVal('quote-heading') ?>">
                                                 <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
                                                 <div class="form__error-text">
                                                     <h3 class="form__error-title">Заголовок сообщения</h3>
@@ -229,7 +235,7 @@
                                         <div class="adding-post__input-wrapper form__textarea-wrapper">
                                             <label class="adding-post__label form__label" for="cite-text">Текст цитаты <span class="form__input-required">*</span></label>
                                             <div class="form__input-section">
-                                                <textarea class="adding-post__textarea adding-post__textarea--quote form__textarea form__input" id="cite-text" placeholder="Текст цитаты"></textarea>
+                                                <textarea class="adding-post__textarea adding-post__textarea--quote form__textarea form__input" id="cite-text" placeholder="Текст цитаты" name="quote-text"><?= getPostVal('quote-text') ?></textarea>
                                                 <button class="form__error-button button" type="button">!<span class="visually-hidden">Информация об ошибке</span></button>
                                                 <div class="form__error-text">
                                                     <h3 class="form__error-title">Заголовок сообщения</h3>
